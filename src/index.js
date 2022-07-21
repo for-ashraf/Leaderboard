247103cbd9de6b08649f3c84c5ecbf9522a676b7
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-07-21 05:58:41 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,50 +1,13 @@
 import './style.css';
+import { createGame, postScore, refreshScores } from './consumeAPI.js';
 
-// api url
-const api_url =
-	"https://employeedetails.free.beeceptor.com/my/api/path";
-
-// Defining async function
-async function getapi(url) {
-	
-	// Storing response
-	const response = await fetch(url);
-	
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-		hideloader();
-	}
-	show(data);
-}
-// Calling that async function
-getapi(api_url);
-
-// Function to hide the loader
-function hideloader() {
-	document.getElementById('loading').style.display = 'none';
-}
-// Function to define innerHTML for HTML table
-function show(data) {
-	let tab =
-		`<tr>
-		<th>Name</th>
-		<th>Office</th>
-		<th>Position</th>
-		<th>Salary</th>
-		</tr>`;
-	
-	// Loop to access all rows
-	for (let r of data.list) {
-		tab += `<tr>
-	<td>${r.name} </td>
-	<td>${r.office}</td>
-	<td>${r.position}</td>
-	<td>${r.salary}</td>		
-</tr>`;
-	}
-	// Setting innerHTML as tab variable
-	document.getElementById("employees").innerHTML = tab;
-}
-
+const form = document.getElementById('enterScore');
+form.onsubmit = postScore;
+document.getElementById('refresh_scores').onclick = refreshScores;
+window.addEventListener('load', () => {
+  const form = document.getElementById('enterScore');
+  if (!localStorage.getItem('gameID')) createGame();
+  const gameID = JSON.parse(localStorage.getItem('gameID'));
+  form.gameID.value = gameID;
+  refreshScores();
+});
